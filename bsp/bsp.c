b42c852037124801c6c02fa2572160aaca9bc669
Juan <juanluiscastagnola@gmail.com>
2014-09-26 10:51:20 -0300

    Toggle 1,2,3 cad 1 ms 0

--- bsp/bsp.c
+++ bsp/bsp.c
@@ -61,7 +61,7 @@ void TIM2_IRQHandler(void) {
 
 	if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET) {
 		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-
+		APP_ISR_1ms();
 		}
 	}
 
--- main.c
+++ main.c
@@ -40,7 +40,7 @@ void Delay(volatile uint32_t nCount) {
 }
 
 void APP_ISR_sw(void){
-	led_toggle(0);
+	//led_toggle(0);
 	led_toggle(1);
 	led_toggle(2);
 	led_toggle(3);
@@ -49,6 +49,7 @@ void APP_ISR_sw(void){
 void APP_ISR_1ms(void){
 
 	static uint16_t count_1ms = 0;
+
 	if (count_1ms++ > 1000) {
 		led_toggle(0);
 		count_1ms = 0;
